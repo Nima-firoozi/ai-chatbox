736af24b781842b579d65ce96525c8186d0b868a
nima <nima.k.firoozi.k@gmail.com>
2026-08-22 15:05:09 +0330

    start project

---
+++ app/components/templates/SignInUp/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SignInUp() {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center'>
+        <div className='wrapper border-2 border-red-600 '>
+            
+        </div>
+    </div>
+  )
+}
+
+export default SignInUp
